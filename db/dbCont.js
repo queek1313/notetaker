4c1aa4962b8a004d570f1e3d92feecf18bd6c052
tony nolan <tonynolan313@gmail.com>
2020-10-23 12:25:19 -0600

    playing with code

---
+++ db/dbCont.js
@@ -0,0 +1,15 @@
+var dbCont = [
+    {
+    "title":"Title 1",
+    "text":"Test test",
+    "id":1
+    },
+    {
+    "title":"Title 2",
+    "text":"Test test",
+    "id":2
+    }
+    
+];
+
+module.exports = dbCont;
