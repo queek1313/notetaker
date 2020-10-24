1f66de0a7b6d3877b34d539935d144cde821cdd6
tony nolan <tonynolan313@gmail.com>
2020-10-24 14:11:35 -0600

    fixed code to add delete and uuid

--- routes/routesApi.js
+++ routes/routesApi.js
@@ -1,34 +1,47 @@
 var db = require("../db/db.json");
 var fs = require("fs");
-const {v4:uuid} = require("uuid");
+const { v4: uuid } = require("uuid");
 const util = require("util");
 
 const writeFileAsync = util.promisify(fs.writeFile);
 const readFileAsync = util.promisify(fs.readFile);
 
-module.exports = function(app){
+module.exports = function (app) {
 
-    
-    app.get("/api/notes", function (req, res){
+
+    app.get("/api/notes", function (req, res) {
         res.json(db)
     });
 
-    app.post("/api/notes", function(req, res){
+    app.post("/api/notes", function (req, res) {
 
-                var note = req.body;
-        
-                var lastId = db[db.length - 1]["id"];
-                var newId = lastId = 1;
-                note["id"] = newId;
-                db.push(note);
-                
-                writeFileAsync("./db/db.json", JSON.stringify(db)).then(function(){
-                    console.log("db.json updated")
-                });
-                res.json(note);
-            });
+        var note = req.body;
 
+        // var lastId = db[db.length - 1]["id"];
+        // var newId = lastId++;
+        note["id"] = uuid();
+        db.push(note);
 
+        writeFileAsync("./db/db.json", JSON.stringify(db)).then(function () {
+        });
+        res.json(note);
+    });
+
+    app.delete("/api/notes/:id", function (req, res) {
+        var chId = req.params.id
+
+        for (var i = 0; i < db.length; i++) {
+            if (chId === db[i].id) {
+                db.splice(i, 1);
+
+                var idJson = JSON.stringify(db)
+                writeFileAsync("./db/db.json", idJson).then(function () {
+
+                })
+            }
+        }
+        res.json(db)
+    })
 
 
 
